636f6f0cec00cd7369fa5e48e2035078c66fef9e
plusdojo2025-nakajimaE2 <nakajima-yuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-16 16:44:49 +0900

    わたしです

--- server/src/main/java/com/example/demo/controller/SearchController.java
+++ server/src/main/java/com/example/demo/controller/SearchController.java
@@ -8,9 +8,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Diaries;
 import com.example.demo.entity.Tag;
 import com.example.demo.repository.CommentsRepository;
+import com.example.demo.repository.DiariesRepository;
 import com.example.demo.repository.ReactionsRepository;
 import com.example.demo.repository.TagsRepository;
 import com.example.demo.repository.UsersRepository;
@@ -45,23 +49,27 @@ public class SearchController {
 			}
 			model.addAttribute("Tags", tags);
 			model.addAttribute("searchedTag", tag);
-			return "search/tag";
+			return "/search/tag";
 		}
 	
 		
 		@GetMapping("/diarypage/{username}/{data}")
-		public String index(Model model) {
+		public String index() {
 			return "diarypage";
 		}
 	
 		@PostMapping("/search/update/")
-		public String update() {
-			return "redirect:/diarypage/{username}/{data}";
+		public String update(@RequestBody Diaries diary, RedirectAttributes redirectAttributes) {
+			redirectAttributes.addFlashAttribute("message", "更新しました");
+			diariesrepository.save(diary);
+			return "redirect:/diarypage/";
 		}
 		
 		@PostMapping("/search/delete/")
-		public String delete() {
-		return "redirect:/diarypage/{username}/{data}";
+		public String del(@RequestBody Diaries diary, RedirectAttributes redirectAttributes) {
+			redirectAttributes.addFlashAttribute("message", "削除しました");
+			diariesrepository.delete(diary);
+			return "redirect:/diarypage/";
 		}
 }
 
