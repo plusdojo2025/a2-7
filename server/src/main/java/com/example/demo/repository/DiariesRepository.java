9bc6852c2e0768e5f3f4cebc8562d100b18bfd7e
plusdojo2025-htoomyatwin <htoo-myat-win-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-18 16:18:17 +0900

    update

--- server/src/main/java/com/example/demo/repository/DiariesRepository.java
+++ server/src/main/java/com/example/demo/repository/DiariesRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Diary;
 
 public interface DiariesRepository extends JpaRepository<Diary, Integer>{
-	
+	List<Diary> findByLoginIdAndDiaryTimeBetween(String loginId, LocalDate startofmonth, LocalDate endofmonth);
 
 }
