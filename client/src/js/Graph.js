8f61a81a47d508c5fd47c176d18f030becf698c9
plusdojo2025-miyazawa <miyazawa-yuuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-22 13:06:49 +0900

    UserDiary、Timelineの削除
    いらないentityを削除しました。

--- client/src/js/Graph.js
+++ client/src/js/Graph.js
@@ -8,8 +8,16 @@ export default class Graph extends React.Component{
         super(props);
         //stateの設定。
         this.state = {
-            diaries:[] ,
+            diaries:[            { stamp: '1', count: 12 , word: 'aa', wordcount: 1},
+            { stamp: '2', count: 10 , word: 'bb', wordcount: 2},
+            { stamp: '3', count: 5 , word: 'cc', wordcount: 3},
+            { stamp: '4', count: 8 , word: 'dd', wordcount: 4},
+            { stamp: '5', count: 15 , word: 'ee', wordcount: 5}] ,
+            activeTab: 'tab1',
             }
+        handleTabChange = (tabId) => {
+            this.setState({ activeTab: tabId });
+        };
     }
     render(){
         const data = this.state.diaries.map((diary, index) => ({
@@ -20,12 +28,12 @@ export default class Graph extends React.Component{
         return (
         <div className="super_graph">
           <div className="tab-container">
-                <input type="radio" name="tabs" id="tab1" className="tab-input" checked></input>
-                <input type="radio" name="tabs" id="tab2" className="tab-input"></input>
             <div className="tab-labels">
                 <div className="tab-group_left">
-                    <label for="tab1" className="tab-label label1">グラフ</label>
-                    <label for="tab2" className="tab-label label2">キーワード分析</label>
+                    <label for="tab1" className={`tab-label label1 ${this.state.activeTab === 'tab1' ? 'active' : ''}`}
+                    onClick={() => this.handleTabChange('tab1')}>グラフ</label>
+                    <label for="tab2" className={`tab-label label2 ${this.state.activeTab === 'tab2' ? 'active' : ''}`}
+                    onClick={() => this.handleTabChange('tab2')}>キーワード分析</label>
                 </div>
                 <div className="tab-group_right">
                     <input className="calender_input" type="date" name="day"></input>
@@ -33,20 +41,20 @@ export default class Graph extends React.Component{
                 </div>
             </div>
           </div>
-          <div className="tab-content content1">
+          <div className="tab-content content1" style={{ display: this.state.activeTab === 'tab1' ? 'block' : 'none' }}>
 
-        	<h1 className="check_results">感情グラフ</h1>
+        	<h1 className="tab-title">感情グラフ</h1>
         	
-        	<h2>スタンプごとの件数の割合</h2>
+        	<h2 className="tab-comments">スタンプごとの件数の割合</h2>
                 <PieChart
                 data={data} // ここにデータ渡すだけで勝手に割合計算してくれる
                 label={({ dataEntry }) => `${dataEntry.title}: ${dataEntry.value}`} // 円グラフ上のラベル
                 labelStyle={{ fontSize: '5px' }}
-                style={{ height: '200px' }}
+                style={{ height: '500px' }}
                 />
           </div>
-          <div className="tab-content content2">
-            <h1 className="check_results">キーワード分析</h1>
+          <div className="tab-content content2" style={{ display: this.state.activeTab === 'tab2' ? 'block' : 'none' }}>
+            <h1 className="tab-title">キーワード分析</h1>
             {/* その他の用途でdiaryを使う */}
             {this.state.diaries.map((diary) => (
                 <div>
