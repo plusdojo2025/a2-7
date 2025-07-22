145c6a0fe7980dc5b787c1543b2f0dbb73c8d873
plusdojo2025-watanabe-miku <watanabe-miku-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-22 13:15:02 +0900

    更新

--- server/src/main/java/com/example/demo/entity/Diary.java
+++ server/src/main/java/com/example/demo/entity/Diary.java
@@ -2,13 +2,18 @@ package com.example.demo.entity;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -39,10 +44,6 @@ public class Diary {
 	private Date diary_time;
 
 
-	public void setLogin_id(int loginId) {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	}
 	
 	@Column(unique = true)
 	private String loginId;
