6b2fa525d537e6e106f3951adc3207ba00ef3874
plusdojo2025-htoomyatwin <htoo-myat-win-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-22 13:09:28 +0900

    nothing change

--- server/src/main/java/com/example/demo/entity/Diary.java
+++ server/src/main/java/com/example/demo/entity/Diary.java
@@ -2,18 +2,13 @@ package com.example.demo.entity;
 
 import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -38,26 +33,16 @@ public class Diary {
 
 	private String sentence;
 	private int stamp;
-	private Timestamp resist_time;	
+	private Timestamp resist_time;
+	
+	
 	private Date diary_time;
-	
-	@Column(unique = true)
-	private String loginId;
-	
-	@OneToMany(mappedBy = "diary"
-			, cascade = CascadeType.ALL
-			, fetch = FetchType.EAGER)
-	private List<Reaction> reactions;
-	
-	@OneToMany(mappedBy = "diary"
-			, cascade = CascadeType.ALL
-			, fetch = FetchType.EAGER)
-	private List<Comment> comments;
-	
-	@OneToMany(mappedBy = "diary"
-			, cascade = CascadeType.ALL
-			, fetch = FetchType.EAGER)
-	private List<Post> posts;
+
+
+	public void setLogin_id(int loginId) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
 	
 
 }
