a72697c450dd7a4cc24dfffd826f316bd4bfdf65
plusdojo2025-htoomyatwin <htoo-myat-win-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-23 15:33:47 +0900

    テーブル　表示

--- server/src/main/java/com/example/demo/repository/DiariesRepository.java
+++ server/src/main/java/com/example/demo/repository/DiariesRepository.java
@@ -10,7 +10,9 @@ import com.example.demo.entity.Diary;
 
 public interface DiariesRepository extends JpaRepository<Diary, Integer>{
 
-	List<Diary> findByLoginIdAndDiaryTimeBetween(String loginId, LocalDate startofmonth, LocalDate endofmonth);
+	//List<Diary> findByLoginIdAndDiaryTimeBetween(String loginId, LocalDate startofmonth, LocalDate endofmonth);
+	List<Diary> findByUserLoginIdAndDiaryTimeBetween(String loginId, LocalDate start, LocalDate end);
+
 	Diary findByDiaryId(int diaryId);
 	List<Diary> findBySentenceLike(String tag);
 	Optional<Diary> findFirstByDiaryTimeAndUser_LoginId(LocalDate localDate,String loginId);
